58fe69f1ec0d78d7dbd64b066e482e4d67d41353
Ollaf <oldrichdvorak268@gmail.com>
2021-05-27 11:55:18 +0200

    basic unit tests

--- packagedelivery/src/test/java/com/homework/packagedelivery/PackageDeliveryApplicationTests.java
+++ packagedelivery/src/test/java/com/homework/packagedelivery/PackageDeliveryApplicationTests.java
@@ -1,10 +1,37 @@
 package com.homework.packagedelivery;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import com.homework.packagedelivery.dto.DeliveryTargetDto;
+import org.junit.jupiter.api.Test;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class PackageDeliveryApplicationTests {
+
+    @Test
+    void addWeightToDtoTest() {
+        float firstPackageWeight = 1.5f;
+        String defaultPostalCode = "10001";
+
+        DeliveryTargetDto dto = new DeliveryTargetDto(firstPackageWeight, defaultPostalCode);
+
+        float secondPackageWeight = 2.3f;
+        dto.addPackageWeight(secondPackageWeight);
+
+        float expectedWeight = 3.8f;
+        assertEquals(expectedWeight, dto.getTotalWeight());
+    }
+
+    @Test
+    void roundingToThreeDecimalsTest() {
+        float firstPackageWeight = 1.5f;
+        String defaultPostalCode = "10001";
+
+        DeliveryTargetDto dto = new DeliveryTargetDto(firstPackageWeight, defaultPostalCode);
+
+        float secondPackageWeight = 2.301999f;
+        dto.addPackageWeight(secondPackageWeight);
+
+        float expectedWeight = 3.802f;
+        assertEquals(expectedWeight, dto.getTotalWeight());
+    }
 }
